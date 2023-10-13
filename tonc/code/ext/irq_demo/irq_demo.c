c807da5c616c775a12f19c234e82960e341cd407
abullard <austinbullard@protonmail.ch>
2023-10-13 14:07:00 -0500

    finished mucking with timer demo, on to interrupts

--- tonc/code/ext/irq_demo/irq_demo.c
+++ tonc/code/ext/irq_demo/irq_demo.c
@@ -5,7 +5,6 @@
 // (20061022 - 20080404, Cearn)
 // === NOTES ===
 
-
 #include <stdio.h>
 #include <tonc.h>
 
@@ -15,64 +14,60 @@ IWRAM_CODE void hbl_grad_direct();
 void vct_wait();
 void vct_wait_nest();
 
-
-CSTR strings[]= 
-{
-	"asm/nested",	"c/direct", 
-	"VCount",		"HBlank"
-};
+CSTR strings[] =
+	{
+		"asm/nested", "c/direct",
+		"VCount", "HBlank"};
 
 // Function pointers to master isrs.
-const fnptr master_isrs[2]= 
-{
-	(fnptr)isr_master,
-	(fnptr)hbl_grad_direct 
-};
+const fnptr master_isrs[2] =
+	{
+		(fnptr)isr_master,
+		(fnptr)hbl_grad_direct};
 
 // VCount interrupt routines.
-const fnptr vct_isrs[2]= 
-{
-	vct_wait, 
-	vct_wait_nest
-};
-
+const fnptr vct_isrs[2] =
+	{
+		vct_wait,
+		vct_wait_nest};
 
 // (1) Uses tonc_isr_master.s'  isr_master() as a switchboard
 void hbl_grad_routed()
 {
-	u32 clr= VERT_DRAW_COUNT/8;
-	pal_bg_mem[0]= RGB15(clr, 0, 31-clr);
+	u32 clr = REG_VCOUNT / 8;
+	pal_bg_mem[0] = RGB15(clr, 0, 31 - clr);
 }
 
 // (2) VCT is triggered at line 80; this waits 40 scanlines
 void vct_wait()
 {
-	pal_bg_mem[0]= CLR_RED;
-	while(VERT_DRAW_COUNT<120);
+	pal_bg_mem[0] = CLR_RED;
+	while (REG_VCOUNT < 120)
+		;
 }
 
 // As vct_wait(), but interruptable by HBlank
 void vct_wait_nest()
 {
-	pal_bg_mem[0]= CLR_RED;
-	REG_IE= IRQ_HBLANK;		// Allow nested hblanks
-	REG_IME= 1;
-	while(VERT_DRAW_COUNT<120);
+	pal_bg_mem[0] = CLR_RED;
+	REG_IE = IRQ_HBLANK; // Allow nested hblanks
+	REG_IME = 1;
+	while (REG_VCOUNT < 120)
+		;
 }
-
 
 int main()
 {
-	u32 bDirect=0, bVctPrio= 0;
+	u32 bDirect = 0, bVctPrio = 0;
 
-	tte_init_chr4c_b4_default(0, BG_CBB(2)|BG_SBB(28));
+	tte_init_chr4c_b4_default(0, BG_CBB(2) | BG_SBB(28));
 	tte_set_drawg((fnDrawg)chr4c_drawg_b4cts_fast);
 	tte_init_con();
 	tte_set_margins(8, 8, 128, 64);
 
-	REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
+	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
 
-	// (3) Initialise irqs; add HBL and VCT isrs 
+	// (3) Initialise irqs; add HBL and VCT isrs
 	// and set VCT to trigger at 80
 	irq_init(master_isrs[0]);
 	irq_add(II_HBLANK, hbl_grad_routed);
@@ -81,39 +76,39 @@ int main()
 
 	irq_add(II_VBLANK, NULL);
 
-	while(1)
+	while (1)
 	{
-		//vid_vsync();
+		// vid_vsync();
 		VBlankIntrWait();
 		key_poll();
 
 		// Toggle HBlank irq
-		if(key_hit(KEY_R))
+		if (key_hit(KEY_R))
 			REG_IE ^= IRQ_HBLANK;
 
 		// Toggle Vcount irq
-		if(key_hit(KEY_L))
+		if (key_hit(KEY_L))
 			REG_IE ^= IRQ_VCOUNT;
 
-		// (4) Toggle between 
+		// (4) Toggle between
 		// asm switchblock + hbl_gradient (red, descending)
 		// or purely hbl_isr_in_c (green, ascending)
-		if(key_hit(KEY_A))
+		if (key_hit(KEY_A))
 		{
 			bDirect ^= 1;
 			irq_set_master(master_isrs[bDirect]);
 		}
 
 		// (5) Switch priorities of HBlank and VCount
-		if(key_hit(KEY_B))
+		if (key_hit(KEY_B))
 		{
-			//irq_set(II_VCOUNT, vct_wait, bVctPrio);
+			// irq_set(II_VCOUNT, vct_wait, bVctPrio);
 			bVctPrio ^= 1;
 			irq_add(II_VCOUNT, vct_isrs[bVctPrio]);
 		}
 
-		tte_printf("#{es;P}ISR#{X:32}: %s\nPrio#{X:32}: %s\nIE#{X:32}: %04X", 
-			strings[bDirect], strings[2+bVctPrio], REG_IE);
+		tte_printf("#{es;P}ISR#{X:32}: %s\nPrio#{X:32}: %s\nIE#{X:32}: %04X",
+				   strings[bDirect], strings[2 + bVctPrio], REG_IE);
 	}
 
 	return 0;
--- tonc/code/ext/tmr_demo/tmr_demo.c
+++ tonc/code/ext/tmr_demo/tmr_demo.c
@@ -5,41 +5,44 @@
 // Press start to start timer.
 //
 // (20031220 -20070809, Cearn)
- 
+
 #include <stdio.h>
 #include <tonc.h>
 
 #include "berk.h"
 
+extern const char _ctype_[];
+const char *__ctype_ptr__ = _ctype_;
 
 void tmr_test()
 {
 	// Overflow every ~1 second:
 	// 0x4000 ticks @ FREQ_1024
 
-	REG_TM2D= -0x4000;			// 0x4000 ticks till overflow
-	REG_TM2CNT= TM_FREQ_1024;	// we're using the 1024 cycle timer	
+	REG_TM2D = -0x4000;		   // 0x4000 ticks till overflow
+	REG_TM2CNT = TM_FREQ_1024; // we're using the 1024 cycle timer
 
 	// cascade into tm3
-	REG_TM3CNT= TM_ENABLE | TM_CASCADE;
+	REG_TM3CNT = TM_ENABLE | TM_CASCADE;
 
-	u32 sec= -1;
+	u32 sec = -1;
 
-	while(1)
+	while (1)
 	{
 		vid_vsync();
 		key_poll();
 
-		if(REG_TM3D != sec)
+		if (REG_TM3D != sec)
 		{
-			sec= REG_TM3D;
-
+			sec = REG_TM3D;
+			tte_printf("#{es;P:24,60}%02d:%02d:%02d",
+					   sec / 3600, (sec % 3600) / 60, sec % 60);
 		}
 
-		if(key_hit(KEY_START))	// pause by disabling timer
+		if (key_hit(KEY_START)) // pause by disabling timer
 			REG_TM2CNT ^= TM_ENABLE;
 
-		if(key_hit(KEY_SELECT))	// pause by enabling cascade
+		if (key_hit(KEY_SELECT)) // pause by enabling cascade
 			REG_TM2CNT ^= TM_CASCADE;
 	}
 }
@@ -47,11 +50,11 @@ void tmr_test()
 int main()
 {
 	// set-up berk font
-	// tte_init_se(0, BG_CBB(0)|BG_SBB(31), 1, 0, 0, &berkFont, se_drawg);
-	// tte_init_con();
-	memcpy16(pal_bg_mem, berkPal, berkPalLen/4);
+	tte_init_se(0, BG_CBB(0)|BG_SBB(31), 1, 0, 0, &berkFont, se_drawg);
+	tte_init_con();
+	memcpy16(pal_bg_mem, berkPal, berkPalLen / 4);
 
-	REG_DISPCNT= DCNT_MODE0 | DCNT_BG0;
+	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0;
 
 	tmr_test();
 
